054a5934ca5c221fb811b2abb927766a7f00f8d4
David Weber <weberdavid91@gmail.com>
2024-04-05 16:46:25 +0200

    refactoring

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import {Routes} from '@angular/router';
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  // {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: '', redirectTo: 'wheel-of-choices', pathMatch: 'full'},
   {path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.routes')},
   {path: 'wheel-of-choices', loadChildren: () => import('./wheel-of-choices/wheel-of-choices.routes')},
   {path: '**', loadChildren: () => import('./shared/error-page/error-page.routes')}
